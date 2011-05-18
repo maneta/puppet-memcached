778642e2c40386cdd943d95abb0a99cd1f5e5da3
Atha Kouroussis <akouroussis@gmail.com>
2011-05-18 17:45:46 -0400

    Removed memcached::config class. Another pass at style guide 1.0 compliance

--- manifests/config/redhat.pp
+++ manifests/config/redhat.pp
@@ -1,9 +1,14 @@
 # Class: memcached::config::redhat
 #
 #
-class memcached::config::redhat inherits memcached::config {
+class memcached::config::redhat {
 	file { '/etc/sysconfig/memcached':
 		ensure  => present,
 		content => template('memcached/memcached.erb.redhat'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => '0644',
+		notify  => Class['memcached::service'],
+		require => Class['memcached::install'],
 	}
 }
